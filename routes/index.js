41439f8e79c3d1914e12658946c3bc99eb888378
Awasjo <awasjomail@gmail.com>
2022-09-22 09:45:06 -0400

    Using bootstrap navbar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. */ //the ejs is going to take this property and inject into the ejs view.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' }); //the ejs is going to take this property and inject into the ejs view.
+  res.render('index', { title: 'Home' }); 
+});
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' }); 
+});
+router.get('/service', function(req, res, next) {
+  res.render('index', { title: 'Services' }); 
+});
+router.get('/product', function(req, res, next) {
+  res.render('index', { title: 'Products' }); 
+});
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Information' }); 
 });
 
 module.exports = router;
